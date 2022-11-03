028ba57761e39b874889838f0ca8224cff54875b
silvio_alvarez <silvio.alvarez@municipiodetandil.edu.ar>
2022-11-03 01:12:16 -0300

    terminado

--- Animales.ts
+++ Animales.ts
@@ -1,24 +1,30 @@
 abstract class Animales {
+    
     public name: string;
     constructor(name: string) {
-      this.name = name;
+        this.name = name;
     }
     public makeSound(): void {
-      console.log (' ');
+        console.log(' ');
     }
 }
 class Cat extends Animales {
-    super() :void{
-      console.log('meow meow\n');
-     }}
-     class Dog extends Animales {
-        super(): void {
-          console.log ('wuff wuff\n');
-        }
+
+    public makeSound(): void {
+        console.log('meow meow\n');
     }
-    
-   let minino: Cat = new Cat('Pocky');
-   minino.makeSound(); 
-   let firulais: Dog = new Dog('Pocky');
-   firulais.makeSound();
-   
+}
+class Dog extends Animales {
+
+    public makeSound(): void {
+        console.log('wuff wuff\n');
+    }
+}
+
+let minino: Cat = new Cat('Pocky');
+minino.makeSound();
+let firulais: Dog = new Dog('Pocky');
+firulais.makeSound();
+
+console.log(minino);
+console.log(firulais);
